58dc0c6834ceb38005bd68b449b335f290b3b2cc
Ryan <rmather@risd.edu>
2014-04-29 03:14:44 -0400

    made the website

---
+++ script.js
@@ -0,0 +1,17 @@
+$(document).ready( function() {
+    $('body').append("<p>This is my first collaboration with
+renaissance painter Leonardo DaVinci, and actor Charlie Chaplin.
+Collaborating with these two has been very enjoyable for us both and I
+am currently accepting offers for collaboration from other prominent
+artists. You can send proposals to hello@ryanflomerfelt.com</p>");  
+});
+
+
+/*$(document).ready(function() {
+$('#artistStatement').click(function() {
+$('#painting').after("<p>" This is my first collaboration with
+renaissance painter Leonardo DaVinci, and actor Charlie Chaplin.
+Collaborating with these two has been very enjoyable for us both and I
+am currently accepting offers for collaboration from other prominent
+artists. You can send proposals to hello@ryanflomerfelt.com"</p>");
+${'.info'}.remove('#artistStatement');     }); });*/
